46ce2a9cae564b64ba1632b9436a9cfcb2845f1d
Kermit-OR <orosas.2006@gmail.com>
2026-05-20 22:45:50 -0700

    Login screen works

--- src/main/java/edu/miracosta/cs112/controllers/MainController.java
+++ src/main/java/edu/miracosta/cs112/controllers/MainController.java
@@ -1,17 +1,45 @@
 package edu.miracosta.cs112.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-
+import javafx.scene.control.Tab;
 
 public class MainController {
 
     @FXML
-    Button hiButton;
+    Tab dietTrackerTab;
 
     @FXML
-    protected void onHelloButtonClicked(ActionEvent actionEvent) {
-        System.out.println("Hello World!");
+    Tab weightTrackerTab;
+
+    @FXML
+    Tab homeworkTrackerTab;
+
+    @FXML
+    Tab pomodoroTimerTab;
+
+    @FXML
+    Tab breakTimerTab;
+
+    @FXML
+    public void initialize() {
+
+    }
+
+    public void disableTabs() {
+        System.out.println("Disabled tabs");
+        if (dietTrackerTab != null) dietTrackerTab.setDisable(true);
+        if (weightTrackerTab != null) weightTrackerTab.setDisable(true);
+        if (homeworkTrackerTab != null) homeworkTrackerTab.setDisable(true);
+        if (pomodoroTimerTab != null) pomodoroTimerTab.setDisable(true);
+        if (breakTimerTab != null) breakTimerTab.setDisable(true);
+    }
+
+    public void enableTabs() {
+        System.out.println("Enabled tabs");
+        if (dietTrackerTab != null) dietTrackerTab.setDisable(false);
+        if (weightTrackerTab != null) weightTrackerTab.setDisable(false);
+        if (homeworkTrackerTab != null) homeworkTrackerTab.setDisable(false);
+        if (pomodoroTimerTab != null) pomodoroTimerTab.setDisable(false);
+        if (breakTimerTab != null) breakTimerTab.setDisable(false);
     }
 }
